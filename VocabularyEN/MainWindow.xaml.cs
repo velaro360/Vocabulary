0819ac758975c9a20e94f87098d74e736a437ca9
velaro360 <grzep360@gmail.com>
2018-11-12 22:47:42 +0100

    simplified code, variable names standardized

--- VocabularyEN/MainWindow.xaml.cs
+++ VocabularyEN/MainWindow.xaml.cs
@@ -1,123 +1,113 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.IO;
-using System.Threading;
+
+using Controls = System.Windows.Controls;
 
 namespace VocabularyEN
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        string[] words;
-        int choice_language;
+        private delegate void Warning_Info(Controls.Label label, string message, SolidColorBrush color);
+
+        /* In each line of file "Słowa.txt" is one word in english and one or more translations for it.
+        1 stands for first word (eng), 2 stands for translation(s) (pl). */
+        int choice_of_language;
+
         char[] pattern;
-        int amount=0;
+        string[] words;
+        int help_size;
+        Warning_Info warning_info;
 
         public MainWindow()
         {
             InitializeComponent();
 
             if (!File.Exists("Słowa.txt")) File.Create("Słowa.txt");
+
+            warning_info = (Controls.Label label, string message, SolidColorBrush color) =>
+            {
+                label.Content = message;
+                label.Foreground = color;
+            };
         }
 
-        private void new_words_b_Click(object sender, RoutedEventArgs e)
+        private void button_add_words_Click(object sender, RoutedEventArgs e)
         {
-            if (new_english.Text != "" && new_polish.Text != "")
+            if (textbox_new_english.Text != "" && textbox_new_polish.Text != "")
             {
                 using (StreamWriter sw = File.AppendText("Słowa.txt"))
                 {
-                    sw.WriteLine((new_english.Text + "," + new_polish.Text).Replace(", ",","));
-                    new_english.Text = ""; new_polish.Text = "";
-                    warning1.Visibility = Visibility.Hidden;
+                    sw.WriteLine((textbox_new_english.Text + "," + textbox_new_polish.Text).Replace(", ",","));
+                    textbox_new_english.Text = ""; textbox_new_polish.Text = "";
+                    label_warning_add.Visibility = Visibility.Hidden;
                     sw.Close();
                 }
             }
 
             else
             {
-                warning1.Visibility = Visibility.Visible;
-                warning1.Content = "Wprowadź słowa";
+                label_warning_add.Visibility = Visibility.Visible;
+                warning_info(label_warning_add, "Wprowadź słowa", new SolidColorBrush(Colors.Red));
             }
         }
 
-        private void translate_tb_KeyDown(object sender, KeyEventArgs e)
+        private void textbox_translate_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key != Key.Enter) return;
+            warning_info(label_warning_translation, "Tłumaczenie niepoprawne", new SolidColorBrush(Colors.Red));
 
-            warning2.Content = "Tłumaczenie niepoprawne";
-            warning2.Foreground = new SolidColorBrush(Colors.Red);
-
-            if (choice_language == 1)
+            if (choice_of_language == 1)
             {
                 for (int i = 1; i < words.Length; i++)
-                {
-                    if (translate_tb.Text == words[i])
-                    {
-                        warning2.Content = "Tłumaczenie poprawne!";
-                        warning2.Foreground = new SolidColorBrush(Colors.Green);
-                    }
-                }
-            }
+                    if (textbox_translate.Text == words[i])
+                        warning_info(label_warning_translation, "Tłumaczenie poprawne!", new SolidColorBrush(Colors.Green));
+            }   
 
             else
             {
-                if (translate_tb.Text == words[0])
-                {
-                    warning2.Content = "Tłumaczenie poprawne!";
-                    warning2.Foreground = new SolidColorBrush(Colors.Green);
-                }
+                if (textbox_translate.Text == words[0]) warning_info(label_warning_translation, "Tłumaczenie poprawne!", new SolidColorBrush(Colors.Green));
             }
 
-            warning2.Visibility = Visibility.Visible;
+            label_warning_translation.Visibility = Visibility.Visible;
         }
 
-        private void random_b_Click(object sender, RoutedEventArgs e)
+        private void button_random_Click(object sender, RoutedEventArgs e)
         {
             if (File.ReadAllLines("Słowa.txt").Count() == 0)
             {
-                warning1.Content = "Brak dodanych słów";
-                warning1.Visibility = Visibility.Visible;
+                warning_info(label_warning_add, "Brak dodanych słów", new SolidColorBrush(Colors.Red));
+                label_warning_add.Visibility = Visibility.Visible;
                 return;
             }
 
-            else warning1.Visibility = Visibility.Hidden;
+            else label_warning_add.Visibility = Visibility.Hidden;
 
-            warning2.Visibility = Visibility.Hidden;
+            label_warning_translation.Visibility = Visibility.Hidden;
 
             string help;
             Random r = new Random();
-            int random_number = r.Next(0, File.ReadLines("Słowa.txt").Count());
-            choice_language = r.Next(1, 3);
+            int random_word = r.Next(0, File.ReadLines("Słowa.txt").Count());
+            choice_of_language = r.Next(1, 3); //If 1 then user try to translate english word into polish, if 2 then vice versa.
 
-            string line = File.ReadLines("Słowa.txt").Skip(random_number).Take(1).First();
+            string line = File.ReadLines("Słowa.txt").Skip(random_word).Take(1).First();
 
             words = line.Split(',');
 
-            random_info2_l.Visibility = Visibility.Visible;
+            label_random_info2.Visibility = Visibility.Visible;
 
-            if (choice_language == 1)
+            if (choice_of_language == 1)
             {
-                random_info2_l.Content = words[0];
+                label_random_info2.Content = words[0];
                 help = words[words.Length - r.Next(0, words.Length - 1) - 1];
             }
 
             else
             {
-                random_info2_l.Content = words[words.Length - r.Next(0, words.Length - 1) - 1];
+                label_random_info2.Content = words[words.Length - r.Next(0, words.Length - 1) - 1];
                 help = words[0];
             }
 
@@ -125,44 +115,30 @@ namespace VocabularyEN
             for (int i = 0; i < help.Length; i++)
                 pattern[i] = '*';
 
-            help_l.Visibility = Visibility.Hidden;
-            help_b.Visibility = Visibility.Visible;
-            translate_tb.Text = "";
-            amount = 0;
+            label_help.Visibility = Visibility.Hidden;
+            button_help.Visibility = Visibility.Visible;
+            textbox_translate.Text = "";
+            help_size = 0;
         }
 
         private void help_b_Click(object sender, RoutedEventArgs e)
         {
             int i;
-            if (choice_language == 1) i = 1; else i = 0;
+            if (choice_of_language == 1) i = 1; else i = 0;
 
             Random r = new Random();
             int pattern_index = r.Next(0, pattern.Length);
 
             while(pattern[pattern_index]!='*')
-            {
                 pattern_index = r.Next(0, pattern.Length);
-            }
 
             pattern[pattern_index] = words[i][pattern_index];
-            char[] pattern2 = new char[pattern.Length * 2];
-            string pattern3 = "";
 
-            int k = 0;
-            for(int j=0; j <= pattern2.Length-2; j = j+2)
-            {
-                pattern2[j] = pattern[k++];
-                pattern2[j + 1] = ' ';
-            }
+            label_help.Content = string.Join(" ", pattern);
+            label_help.Visibility = Visibility.Visible;
 
-            for (int j = 0; j < pattern2.Length; j++)
-                pattern3 += pattern2[j];
-
-            help_l.Content = pattern3;
-            help_l.Visibility = Visibility.Visible;
-
-            amount++;
-            if (amount == pattern.Length) help_b.Visibility = Visibility.Hidden;
+            help_size++;
+            if (help_size == pattern.Length) button_help.Visibility = Visibility.Hidden;
         }
     }
 }
